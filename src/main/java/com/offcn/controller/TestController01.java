5842ab2f61ed91cb627d1c189268ed197a3bd87c
xhc <myqq@qq.com>
2021-04-19 21:51:20 +0800

    编写了一个测试方法

---
+++ src/main/java/com/offcn/controller/TestController01.java
@@ -0,0 +1,14 @@
+package com.offcn.controller;
+
+
+public class TestController01 {
+
+
+    public void test(){
+        boolean flag = true;
+        if(flag){
+            System.out.println("----");
+        }
+        System.out.println("123");
+    }
+}
